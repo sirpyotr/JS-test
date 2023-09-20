065ff19c9a08c059c7bef3c3691179a64b0e9d46
sirpyotr <spk.peter.savitskiy@gmail.com>
2023-09-20 15:43:25 +0300

    idk

---
+++ hundred-prompt/script.js
@@ -0,0 +1,11 @@
+// let num;
+
+// for(;num <=100 && num;) {
+//     num = prompt("Enter a number greater than 100")
+// }
+
+let num;
+
+while(num <= 100 && num) {
+    num = prompt("Enter number", 0);
+}
---
+++ launch-countdown/script.js
@@ -0,0 +1,41 @@
+// const output = document.querySelector(".output");
+
+// // output.innerHTML = ' ';
+
+// // output.appendChild(para); — appends the paragraph to the output <div>.
+// // para.textContent = — makes the text inside the paragraph equal to whatever you put on the right-hand side, after the equals sign.
+// // function coundown() {
+// //     for(i = 10; i >= 0; i--) {
+// //         const para = document.createElement("p");
+// //         if (i === 10) {
+// //             para.textContent = "Countdown 10";
+            
+// //         } else if( i === 0) {
+// //             para.textContent = "Blast off"
+// //         } else {
+// //             para.textContent = i
+// //         }
+    
+// //         output.appendChild(para)
+// //     }
+// // }
+
+// // coundown()
+
+
+// let i = 10;
+
+// while (i >= 0) {
+//   const para = document.createElement('p');
+//   if (i === 10) {
+//     para.textContent = `Countdown ${i}`;
+//   } else if (i === 0) {
+//     para.textContent = 'Blast off!';
+//   } else {
+//     para.textContent = i;
+//   }
+
+//   output.appendChild(para);
+
+//   i--;
+}
